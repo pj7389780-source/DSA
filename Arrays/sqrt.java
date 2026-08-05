874eb33d8297d013ce2630ad9bbc9b9624f53cf8
PARAS JAIN <pj7389780@gmail.com>
2026-08-05 22:41:27 +0530

    sqrt

---
+++ Arrays/sqrt.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int mySqrt(int x) {
+        if(x<2) return x;
+        int low=1,high=x,ans=0;
+        while(low<=high){
+            int mid = low+((high-low)/2);
+            if((long)mid*mid==x) return mid;
+            else if((long)mid*mid<x){
+                low=mid+1;
+                ans=mid;
+            }else{
+                high=mid-1;
+            }
+
+        }
+        return ans;
+    }
+}
